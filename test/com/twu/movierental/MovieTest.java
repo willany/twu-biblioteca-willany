a10c94cfe349580374ef5442bbed1d8df2002c55
willany <willany_lp@hotmail.com>
2018-03-13 18:00:20 -0300

    add tests for a unrating movie

--- test/com/twu/movierental/MovieTest.java
+++ test/com/twu/movierental/MovieTest.java
@@ -8,29 +8,35 @@ import static org.junit.Assert.assertEquals;
 public class MovieTest {
 
     Movie movie;
+    Movie movieWithoutRating;
 
     @Before
     public void setUp() {
         movie = new Movie("Um amor para recordar", 2000, "Nicholas Sparks", 10);
+        movieWithoutRating = new Movie("Um amor para recordar", 2000, "Nicholas Sparks");
     }
 
     @Test
     public void validateMovieDetailsShouldHaveName(){
         assertEquals("Um amor para recordar", movie.getName());
+        assertEquals("Um amor para recordar", movieWithoutRating.getName());
     }
 
     @Test
     public void validateMovieDetailsShouldHaveYear(){
         assertEquals(2000, movie.getYear());
+        assertEquals(2000, movieWithoutRating.getYear());
     }
 
     @Test
     public void validateMovieDetailsShouldHaveDirector(){
         assertEquals("Nicholas Sparks", movie.getDirector());
+        assertEquals("Nicholas Sparks", movieWithoutRating.getDirector());
     }
 
     @Test
     public void validateMovieDetailsShouldHaveRating(){
         assertEquals(10, movie.getRating());
+        assertEquals(0, movieWithoutRating.getRating());
     }
 }
